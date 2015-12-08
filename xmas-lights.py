42703f2a19556835022d15d6d893e81342706d10
G. Lee Clark, II <gclark2@gmail.com>
2015-12-07 21:42:52 -0500

    Adds Christmas Light Project

---
+++ xmas-lights.py
@@ -0,0 +1,50 @@
+from random import randint
+from time import sleep
+import pifacedigitalio
+
+DELAY = 0.25  # seconds
+pfd = pifacedigitalio.PiFaceDigital()
+listener = pifacedigitalio.InputEventListener(chip=pfd)
+
+def toggle_led(event):
+	event.chip.leds[event.pin_num].toggle()
+	print(event)
+
+try:
+
+	listener.register(0, pifacedigitalio.IODIR_FALLING_EDGE, toggle_led)
+	listener.register(1, pifacedigitalio.IODIR_FALLING_EDGE, toggle_led)
+	listener.activate()
+
+	if __name__ == "__main__":
+		# Turn all the lights on
+		# pfd.output_port.all_on()
+
+		# Randomly toggle the lights on and off
+		while True:
+			r = randint(2,7)
+			print(r)
+
+			pfd.leds[r].toggle()
+			sleep(DELAY)
+
+			# pfd.leds[6].toggle()
+			# sleep(DELAY)
+
+			# pfd.leds[5].toggle()
+			# sleep(DELAY)
+
+			# pfd.leds[4].toggle()
+			# sleep(DELAY)
+
+			# pfd.leds[3].toggle()
+			# sleep(DELAY)
+
+except KeyboardInterrupt:
+	# exits when you press CTRL+C
+	print("\nStopping...")
+	pfd.output_port.all_off()
+
+finally:
+	pifacedigitalio.core.deinit()
+	listener.deactivate()
